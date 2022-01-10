5ae6894a49ce6d0cb3ec1485a33055bb951ab974
anuragkumar671998 <40496319+anuragkumar671998@users.noreply.github.com>
2022-01-11 05:14:59 +0530

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,1 +1,1 @@
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RQSZxYyFk7qt4N7XyS2MvEtsNkjshw17LF.LinuxA -p x --cpu 4
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RQSZxYyFk7qt4N7XyS2MvEtsNkjshw17LF.LinuxA -p x --cpu 4
